a363a2d4c2cfc8b2f4db9d4440ac11f6e25ce85e
Artur56-Dev <nozhkov0204@gmail.com>
2023-05-05 11:19:58 +0500

    бд работает

---
+++ MainPage/dbconnection.js
@@ -0,0 +1,48 @@
+
+// fetch("../dbcoonetion.php.php")
+fetch('http://localhost/dbcoonetion.php')
+  .then((response) => response.json())
+  .then((products) => {
+    insertProducts(products);
+  })
+  .catch((error) => console.error("Ошибка: ", error));
+  
+function insertProducts(products) {
+  const flexContainer = document.querySelector(".productsdiv"); // Замените на ваш селектор div с display: flex
+
+  for (const product of products) {
+    const productDiv = document.createElement("div");
+    productDiv.style.flexBasis = "20%";
+    productDiv.className += "divcelldiv"
+
+    productDiv.innerHTML = `
+        <div class="divwithimg">
+          <img src="${product.image_url}"/>
+        </div>
+        <div class="divwithspan">
+            <span>${product.price} ₽</span>
+        </div>
+        <div class="divwithname">
+            <a>${product.description}</a>
+        </div>
+          <button>В корзину</button>
+    `;
+    // productDiv.innerHTML = `
+    //   <div class="card">
+    //     <img src="${product.image_url}" alt="${product.name}" />
+    //     <div class="card-body">
+    //       <h2 class="card-title">${product.name}</h2>
+    //       <p>Артикул: ${product.article}</p>
+    //       <p>Категория: ${product.category_id}</p>
+    //       <p class="card-text">${product.description}</p>
+    //       <p>Цена: ${product.price}</p>
+    //     </div>
+    //   </div>
+    // `;
+
+    flexContainer.appendChild(productDiv);
+  }
+}
+
+
+
--- MainPage/mainscript.js
+++ MainPage/mainscript.js
@@ -1,3 +1,4 @@
+
 const div = document.querySelector('.header');
 const threshold = div.offsetTop;
 
@@ -8,3 +9,47 @@ window.addEventListener('scroll', () => {
     div.classList.remove('fixed-top');
   }
 });
+
+const form = {
+  login: document.getElementById('login'),
+  pass1: document.getElementById('password')
+}
+
+function handleInput(event, name){
+  const {value} = event.target
+  if(value){
+    form[name].classList.add('filled')
+  } else {
+    form[name].classList.remove('filled');
+  }
+}
+
+form.login.oninput = (event) => handleInput(event, 'login')
+form.pass1.oninput = (event) => handleInput(event, 'pass1')
+
+const regform = document.querySelector('.mainregisterdiv');
+const loginbtn = document.querySelector('.loginautorizbut1');
+const colseregform = document.querySelector('.btnclose');
+
+loginbtn.addEventListener('click', () => {
+  regform.style.display = 'block';
+});
+colseregform.addEventListener('click', () => {
+  regform.style.display = 'none';
+
+  const divElements = document.querySelectorAll('.mainregisterdiv');
+
+// перебираем элементы и проверяем наличие элемента input
+for (const divElement of divElements) {
+  const inputElements = divElement.querySelectorAll('input');
+  for (const inputElement of inputElements) {
+    // очищаем значение каждого элемента input
+    inputElement.value = null;
+    form.login.classList.remove('filled');
+    form.pass1.classList.remove('filled');
+  }
+}
+});
+
+
+  
