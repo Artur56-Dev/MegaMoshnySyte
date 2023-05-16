9952fd1d3e1dc40f8e666cfd8c21e18603fedf17
Artur56-Dev <nozhkov0204@gmail.com>
2023-05-16 13:54:05 +0500

    рабочая корзина

---
+++ Cart/index.php
@@ -0,0 +1,95 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Cart/style.css">
+    <script src="  https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Корзина</title>
+</head>
+
+<body>
+    <div class="maindiv">
+        <div>
+            <a class="back" href="../">Назад</a>
+            <h1 style="margin-top: 20px; margin-bottom:20px;">Корзина</h1>
+            <div class="aa1">
+                <div class="aa1-bb1">
+                </div>
+                <div class="aa1-bb2">
+                    <div class="aa-bb2-div">
+                        <button class="aa1-bb2-cc1">
+                            <p>Заказать</p>
+                        </button>
+                        <h3>Ваша корзина</h3>
+                        <div class="aa1-bb2-cc1-div1 grayfont">
+                            <p id="quantityproduct">Товары, 0 шт.</p>
+                            <p id='full_price1'>0 ₽</p>
+                        </div>
+                        <div class="aa1-bb2-cc1-div1">
+                            <h2>Итого</h2>
+                            <h2 id='full_price2'>0 ₽</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div>
+            <div class="foot-div1">
+                <span>Маркетплейс</span>
+                <a>Контакты</a>
+                <a>Развитие</a>
+                <a>Партнерская программа</a>
+                <a></a>
+            </div>
+            <div class="foot-div1">
+                <span>Покупателю</span>
+                <a>Помощь покупатлю</a>
+                <a>Доставка</a>
+                <a>Примерка</a>
+                <a>Оплата</a>
+                <a>Возврат</a>
+                <a>Промокоды</a>
+            </div>
+            <div class="foot-div1">
+                <span>Работать с компанией</span>
+                <a>Стать продавцом</a>
+                <a>Стать поставщиком</a>
+                <a>Открыть пункт выдачи</a>
+            </div>
+            <div class="foot-div1">
+                <span>Правовая информация</span>
+                <a>Условия использования сайта</a>
+                <a>Политика обработки персональных данных</a>
+                <a>Условия заказа и доставки</a>
+                <a>Правила сервиса «закажи и забери»</a>
+            </div>
+            <div class="foot-div2">
+                <img src="../Icons/QR-code(1).svg" height="130px" width="130px" style="border-radius: 10px" />
+                <div>
+                    <button>
+                        <img src="../Icons/free-icon-vkontakte-4494517.png" />
+                    </button>
+                    <button>
+                        <img src="../Icons/free-icon-odnoklassniki-3670250.png" />
+                    </button>
+                    <button>
+                        <img src="../Icons/free-icon-telegram-2111646.png" />
+                    </button>
+                </div>
+                <div>Нас там нет и не будет</div>
+                <div style="font-size: 2pt">Сайт нарушает все авторские права</div>
+            </div>
+        </div>
+    </footer>
+    <script src="/Cart/script.js"></script>
+</body>
+
+</html>
